2416d8a328acf96a72fc2e9666da669c491f0c36
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2019-11-19 15:00:47 +0900

    함수 작성

---
+++ kindLinuxy/kindLinuxy/commandManager.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"userInfo.c"
+#include"userInput.c"
+#include"userOutput.c"
+
+_manage() {
+
+}
--- kindLinuxy/kindLinuxy/kindLinuxy.c
+++ kindLinuxy/kindLinuxy/kindLinuxy.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include"commandManager.c"
 
 int main() {
+	_manage();
 
-
+	return 0;
 }
