c7c9f3a6e71f31782885b6695b6661dc1c39075b
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2019-11-30 17:19:58 +0900

    info 형태 변경

--- kindLinuxy/kindLinuxy/commandManager.c
+++ kindLinuxy/kindLinuxy/commandManager.c
@@ -13,12 +13,12 @@ void _printExitMsg() {
 }
 
 void _manage() {	//exit 말고 atexit 써야할듯?
-	char* info[4];	// 일단 넉넉하게 이렇게...
-//	atexit(_printExitMsg);
+	char** info;	// 일단 넉넉하게 이렇게...
+	atexit(_printExitMsg);
 
 	while(!doneflag){
 		_printInfo();
 		_command(info);
-	//	work(info);
+		work(info);
 	}
 }
