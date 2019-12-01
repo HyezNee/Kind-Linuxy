f1ace486de32cc9e4cf02043066162e27816da9c
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2019-12-01 23:17:27 +0900

    effective 사용자 이름이 출력될 예정

--- kindLinuxy/kindLinuxy3/userInfo.c
+++ kindLinuxy/kindLinuxy3/userInfo.c
@@ -1,14 +1,15 @@
 #include "header.h"
 
 void _printInfo() {//execl,getuid	
+	char buf[256] = "";
 	printf("=====================================================\n");
 	printf("<친절한 리눅씨>\n");
 	printf("현재 경로 : ");
 	char cwd[100];
 	getcwd(cwd, sizeof(cwd));
 	printf("%s\n", cwd);
-	printf("사용자 : ");
-	printf("%s", getlogin());
+	cuserid(buf);
+	printf("사용자 : %s\n",buf);
 	printf("-----------------------------------------------------\n");
 
 
