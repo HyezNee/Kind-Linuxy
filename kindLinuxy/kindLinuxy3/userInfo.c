414f4bc73a86fadf4a84676496fc961dd302d034
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2019-12-01 23:14:47 +0900

    userinfo 줄정리

--- kindLinuxy/kindLinuxy3/userInfo.c
+++ kindLinuxy/kindLinuxy3/userInfo.c
@@ -6,7 +6,7 @@ void _printInfo() {//execl,getuid
 	printf("현재 경로 : ");
 	char cwd[100];
 	getcwd(cwd, sizeof(cwd));
-	printf("%s", cwd);
+	printf("%s\n", cwd);
 	printf("사용자 : ");
 	printf("%s", getlogin());
 	printf("-----------------------------------------------------\n");
