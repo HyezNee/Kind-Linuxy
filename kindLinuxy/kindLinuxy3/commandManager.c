9b00c4abcde14206df79116d416d67d47c21d376
HyezNee <46377695+HyezNee@users.noreply.github.com>
2019-12-01 18:18:43 +0900

    command+userInput+userOutput(목록, 경로이동) 합침

---
+++ kindLinuxy/kindLinuxy3/commandManager.c
@@ -0,0 +1,32 @@
+#include <signal.h>
+#include <stdlib.h>
+#include "header.h"
+
+static volatile sig_atomic_t doneflag = 0;
+
+static void handler(int sig) {
+	doneflag = 1;
+}
+
+void _printExitMsg() {
+	printf("친절한 리눅씨를 종료합니다.");
+}
+
+void _manage() {	//exit 말고 atexit 써야할듯?
+	char* info[4];	// 일단 넉넉하게 이렇게...
+	atexit(_printExitMsg);
+
+	while(!doneflag){
+		_printInfo();
+		_command(info);
+		_work(info);
+		printf("\n");
+	}
+}
+
+
+int main(void){
+	// printf("이것은 한글이다.")
+	_manage();
+	return 0;
+}
---
+++ kindLinuxy/kindLinuxy3/userInfo.c
@@ -0,0 +1,6 @@
+#include "header.h"
+
+void _printInfo() {//execl,getuid	박소연 작성
+
+
+}
